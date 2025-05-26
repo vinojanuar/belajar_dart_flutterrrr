ace32fdacaecca37a97fba24226a47bbdb7aa4a1
vinojanuar <vinojanuar839@gmail.com>
2025-05-25 21:48:03 -0700

    Tugas 6

--- lib/meet6/Tugas6.dart
+++ lib/meet6/Tugas6.dart
@@ -11,6 +11,7 @@ class _TugasEnamState extends State<TugasEnam> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 248, 248, 248),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
@@ -28,27 +29,63 @@ class _TugasEnamState extends State<TugasEnam> {
             ),
             const SizedBox(height: 36),
 
+            // Baris tombol Phone Number & Email
             Container(
               width: double.infinity,
-              height: 50,
-              color: const Color.fromARGB(255, 230, 166, 161),
-              padding: const EdgeInsets.symmetric(horizontal: 8),
+              height: 60,
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 223, 221, 221),
+                borderRadius: const BorderRadius.all(Radius.circular(10)),
+              ),
+              padding: const EdgeInsets.symmetric(horizontal: 10.0),
               child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: const Text("Use Phone Number"),
+                  SizedBox(
+                    width: 151,
+                    height: 48,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                      ),
+                      child: const Text(
+                        "Phone Number",
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
                   ),
-                  ElevatedButton(onPressed: () {}, child: const Text("Email")),
+                  SizedBox(
+                    width: 151,
+                    height: 48,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        shadowColor: Colors.transparent,
+                        backgroundColor: Color.fromARGB(255, 223, 221, 221),
+                      ),
+                      onPressed: () {},
+                      child: Text(
+                        "Email",
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                  ),
                 ],
               ),
             ),
 
+            // Input Phone Number
             const SizedBox(height: 40),
             const Text("Phone Number"),
             const SizedBox(height: 10),
             TextField(
+              keyboardType: TextInputType.phone,
               decoration: InputDecoration(
                 hintText: "Enter your phone number",
                 border: OutlineInputBorder(
@@ -57,6 +94,7 @@ class _TugasEnamState extends State<TugasEnam> {
               ),
             ),
 
+            // Input Password
             const SizedBox(height: 40),
             const Text("Password"),
             const SizedBox(height: 10),
@@ -70,16 +108,19 @@ class _TugasEnamState extends State<TugasEnam> {
               ),
             ),
 
+            // Tombol Request OTP
             const SizedBox(height: 20),
-
             ElevatedButton(
               onPressed: () {
                 // aksi request OTP
               },
               style: ElevatedButton.styleFrom(
-                backgroundColor: const Color(0xff015551),
+                backgroundColor: const Color(0xff21BDCA),
                 elevation: 0,
                 minimumSize: const Size(double.infinity, 56),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
               ),
               child: const Text(
                 "Request OTP",
@@ -89,6 +130,113 @@ class _TugasEnamState extends State<TugasEnam> {
                   color: Colors.white,
                 ),
               ),
+            ),
+
+            // Teks tambahan
+            const SizedBox(height: 40),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: Divider(color: const Color.fromARGB(255, 70, 69, 69)),
+                ),
+                Expanded(
+                  child: Text(
+                    "Or Sign In With",
+                    style: TextStyle(fontSize: 14, color: Colors.grey),
+                  ),
+                ),
+                Expanded(
+                  child: Divider(color: const Color.fromARGB(255, 74, 73, 73)),
+                ),
+              ],
+            ),
+
+            SizedBox(height: 40),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  width: 155,
+                  height: 50,
+
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 174, 169, 169),
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 12),
+                        child: Image.asset(
+                          "assets/images/iconGoogle.png",
+                          width: 24,
+                          height: 25,
+                        ),
+                      ),
+                      const SizedBox(width: 10),
+                      const Text(
+                        "Google",
+                        style: TextStyle(fontSize: 14, color: Colors.black87),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  width: 155,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 174, 169, 169),
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 12),
+                        child: Image.asset(
+                          "assets/images/logofb.png",
+                          width: 24,
+                          height: 25,
+                        ),
+                      ),
+                      const SizedBox(width: 10),
+                      const Text(
+                        "Facebook",
+                        style: TextStyle(fontSize: 14, color: Colors.black87),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+
+            const SizedBox(height: 40),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text(
+                  "Don't have an account? ",
+                  style: TextStyle(fontSize: 14, color: Colors.black87),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    // TODO: Navigate to Sign Up screen
+                  },
+                  child: const Text(
+                    "Sign Up",
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Color(0xff21BDCA),
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
