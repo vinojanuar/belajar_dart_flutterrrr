d4b786d095214034e169838709e1c0be9db3b59f
vinojanuar <vinojanuar839@gmail.com>
2025-06-05 00:24:16 -0700

    tugas11

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.belajar_dart_flutter"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "29.0.13113456"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
--- lib/tugas11/model_kost.dart
+++
@@ -1,3 +0,0 @@
-class Siswa{
-  fina
-}
